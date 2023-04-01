49820fd1fa2e7876ad0ccf26d1d7e75351651be6
Druvasena Reddy Naini <nainidruvasenareddy3737@gmail.com>
2023-04-02 03:13:50 +0530

    Amazoff

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -19,30 +19,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("orders")
 public class OrderController {
 
-
+    @Autowired
+    OrderService orderService;
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order){
-
+        orderService.addOrder(order);
         return new ResponseEntity<>("New order added successfully", HttpStatus.CREATED);
     }
 
     @PostMapping("/add-partner/{partnerId}")
     public ResponseEntity<String> addPartner(@PathVariable String partnerId){
-
+        orderService.addPartner(partnerId);
         return new ResponseEntity<>("New delivery partner added successfully", HttpStatus.CREATED);
     }
 
     @PutMapping("/add-order-partner-pair")
     public ResponseEntity<String> addOrderPartnerPair(@RequestParam String orderId, @RequestParam String partnerId){
-
         //This is basically assigning that order to that partnerId
+        orderService.addOrderPartnerPair(orderId,partnerId);
         return new ResponseEntity<>("New order-partner pair added successfully", HttpStatus.CREATED);
     }
 
     @GetMapping("/get-order-by-id/{orderId}")
     public ResponseEntity<Order> getOrderById(@PathVariable String orderId){
 
-        Order order= null;
+        Order order= orderService.getOrderById(orderId);
         //order should be returned with an orderId.
 
         return new ResponseEntity<>(order, HttpStatus.CREATED);
@@ -51,7 +52,7 @@ public class OrderController {
     @GetMapping("/get-partner-by-id/{partnerId}")
     public ResponseEntity<DeliveryPartner> getPartnerById(@PathVariable String partnerId){
 
-        DeliveryPartner deliveryPartner = null;
+        DeliveryPartner deliveryPartner = orderService.getPartnerById(partnerId);
 
         //deliveryPartner should contain the value given by partnerId
 
@@ -62,8 +63,9 @@ public class OrderController {
     public ResponseEntity<Integer> getOrderCountByPartnerId(@PathVariable String partnerId){
 
         Integer orderCount = 0;
-
         //orderCount should denote the orders given by a partner-id
+
+        orderCount = orderService.getOrderCountByPartnerId(partnerId);
 
         return new ResponseEntity<>(orderCount, HttpStatus.CREATED);
     }
@@ -74,6 +76,8 @@ public class OrderController {
 
         //orders should contain a list of orders by PartnerId
 
+        orders = orderService.getOrdersByPartnerId(partnerId);
+
         return new ResponseEntity<>(orders, HttpStatus.CREATED);
     }
 
@@ -82,6 +86,7 @@ public class OrderController {
         List<String> orders = null;
 
         //Get all orders
+        orders = orderService.getAllOrders();
         return new ResponseEntity<>(orders, HttpStatus.CREATED);
     }
 
@@ -90,16 +95,17 @@ public class OrderController {
         Integer countOfOrders = 0;
 
         //Count of orders that have not been assigned to any DeliveryPartner
-
+        countOfOrders = orderService.getCountOfUnassignedOrders();
         return new ResponseEntity<>(countOfOrders, HttpStatus.CREATED);
     }
 
-    @GetMapping("/get-count-of-orders-left-after-given-time/{partnerId}")
+    @GetMapping("/get-count-of-orders-left-after-given-time/{time}/{partnerId}")
     public ResponseEntity<Integer> getOrdersLeftAfterGivenTimeByPartnerId(@PathVariable String time, @PathVariable String partnerId){
 
-        Integer countOfOrders = 0;
+        int countOfOrders = 0;
 
         //countOfOrders that are left after a particular time of a DeliveryPartner
+        countOfOrders = orderService.getOrdersLeftAfterGivenTimeByPartnerId(time,partnerId);
 
         return new ResponseEntity<>(countOfOrders, HttpStatus.CREATED);
     }
@@ -109,6 +115,7 @@ public class OrderController {
         String time = null;
 
         //Return the time when that partnerId will deliver his last delivery order.
+        time = orderService.getLastDeliveryTimeByPartnerId(partnerId);
 
         return new ResponseEntity<>(time, HttpStatus.CREATED);
     }
@@ -118,7 +125,7 @@ public class OrderController {
 
         //Delete the partnerId
         //And push all his assigned orders to unassigned orders.
-
+        orderService.deletePartnerById(partnerId);
         return new ResponseEntity<>(partnerId + " removed successfully", HttpStatus.CREATED);
     }
 
@@ -127,7 +134,7 @@ public class OrderController {
 
         //Delete an order and also
         // remove it from the assigned order of that partnerId
-
+        orderService.deleteOrderById(orderId);
         return new ResponseEntity<>(orderId + " removed successfully", HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/driver/OrderRepository.java
@@ -0,0 +1,87 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class OrderRepository {
+
+    HashMap<String,Order> orderDb = new HashMap<>();
+    HashMap<String,DeliveryPartner> partnerDb = new HashMap<>();
+    HashMap<String, List<String>> orderPartnerPairDb = new HashMap<>();
+
+    public void addOrder(Order order){
+        orderDb.put(order.getId(),order);
+    }
+    public void addPartner(String partnerId){
+        partnerDb.put(partnerId,new DeliveryPartner(partnerId));
+        orderPartnerPairDb.put(partnerId,new ArrayList<>());
+    }
+    public void addOrderPartnerPair(String orderId,String partnerId){
+        partnerDb.get(partnerId).setNumberOfOrders(partnerDb.get(partnerId).getNumberOfOrders()+1);
+        List<String> partnerOrders;
+        if(orderPartnerPairDb.containsKey(partnerId))
+            partnerOrders = orderPartnerPairDb.get(partnerId);
+        else
+            partnerOrders = new ArrayList<>();
+        partnerOrders.add(orderId);
+        orderPartnerPairDb.put(partnerId,partnerOrders);
+    }
+    public Order getOrderById(String orderId){
+        return orderDb.get(orderId);
+    }
+    public DeliveryPartner getPartnerById(String partnerId){
+        return partnerDb.get(partnerId);
+    }
+    public int getOrderCountByPartnerId(String partnerId){
+        return partnerDb.get(partnerId).getNumberOfOrders();
+    }
+    public List<String> getOrdersByPartnerId(String partnerId){
+        return orderPartnerPairDb.get(partnerId);
+    }
+    public List<String> getAllOrders(){
+        return new ArrayList<>(orderDb.keySet());
+    }
+    public int getCountOfUnassignedOrders(){
+        int assignedOrdersCount = 0;
+        for(List<String> orderList: orderPartnerPairDb.values()){
+            assignedOrdersCount+=orderList.size();
+        }
+        return orderDb.size() - assignedOrdersCount;
+    }
+    public int getOrdersLeftAfterGivenTimeByPartnerId(String time,String partnerId){
+        int ordersLeftAfterGivenTime = 0;
+        int givenTime = Integer.parseInt(time.split(":")[0])*60;
+        givenTime += Integer.parseInt(time.split(":")[1]);
+        for(String orderId: orderPartnerPairDb.get(partnerId)){
+            if(givenTime <= orderDb.get(orderId).getDeliveryTime()) ordersLeftAfterGivenTime++;
+        }
+        return ordersLeftAfterGivenTime;
+    }
+    public int getLastDeliveryTimeByPartnerId(String partnerId){
+        int mx = 0;
+        for(String orderId : orderPartnerPairDb.get(partnerId)){
+            mx = Math.max(mx,orderDb.get(orderId).getDeliveryTime());
+        }
+        return mx;
+    }
+    public void deletePartnerById(String partnerId){
+        orderPartnerPairDb.remove(partnerId);
+        partnerDb.remove(partnerId);
+    }
+    public void deleteOrderById(String orderId){
+        orderDb.remove(orderId);
+        for(List<String> orderList: orderPartnerPairDb.values()){
+            for(int i=0;i<orderList.size();i++){
+                if(orderList.get(i).equals(orderId)){
+                    orderList.remove(i);
+                    break;
+                }
+            }
+        }
+    }
+}
